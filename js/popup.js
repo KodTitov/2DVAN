88f6f08dc432a6e28d919b8944a8de68d1ffe6ed
KodTitov <kodtitov@protonmail.com>
2020-11-22 12:45:35 +0200

    addBackEndFormMaster

--- js/popup.js
+++ js/popup.js
@@ -8,3 +8,14 @@ $('.close-popup-client').click(function() {
     $('.popup-bg-client').fadeOut(900);
     $('html').removeClass('no-scroll');
 });
+
+$('.open-popup-master').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg-master').fadeIn(900);
+    $('html').addClass('no-scroll');
+});
+
+$('.close-popup-master').click(function() {
+    $('.popup-bg-master').fadeOut(900);
+    $('html').removeClass('no-scroll');
+});
